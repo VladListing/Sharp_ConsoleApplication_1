a451593379a4b9aa5d5346150e96ecddf61a7cad
VladListing <chvch@mail.ru>
2017-03-05 19:07:26 +0700

    удаление зазора в коде

--- Program.cs
+++ Program.cs
@@ -12,11 +12,6 @@ namespace Sharp_ConsoleApplication_1
         {
             Console.WriteLine("проверочный текст");
 
-
-
-
-
-
             Console.WriteLine("изменение 1");
 
             Console.WriteLine("изменение 2");
