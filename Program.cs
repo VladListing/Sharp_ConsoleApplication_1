f5a6be062ef3c4a0c448579b814a1954b926f1df
VladListing <chvch@mail.ru>
2017-03-05 16:33:14 +0700

    добавление в код изминения 1

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ namespace Sharp_ConsoleApplication_1
         {
             Console.WriteLine("проверочный текст");
 
+            Console.WriteLine("изменение 1");
+
             Console.ReadLine();
         }
     }
